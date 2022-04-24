f551b383e2afcfda792f47a38791926ca7394819
ace-krypton <ramizna@code.edu.az>
2022-04-24 19:48:46 +0200

    Added `category creating`

--- AddCategory.cpp
+++ AddCategory.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <unordered_map>
 #include "header.hpp"
 
@@ -8,18 +9,20 @@
  * @return void
  */
 auto AddCategory::add_category() -> void {
-    std::unordered_map<int, std::string> categories;  //Unordered map for storing categories and their IDs
+    //Unordered map for storing categories and their passwords
+    std::unordered_map<std::string, std::vector<std::string>> categories;
     std::string category_input;  //User input
-    int count = 0;  //Category ID or we can say "Key"
+    std::vector<std::string> passwords {"1l1k3l1nux", "ramiz852"};
     std::cout << "+-----------------------------------------------------------------------------------+" << std::endl;
     std::cout << "      Adds a new category to be used with creating new passwords\n"
                  "                      For stop adding type \"quit\"\n" << std::endl;
 
-    while (category_input != "quit") {
+    while (category_input != "quit") {  //If user input different from quit, it adds
         std::cout << "Enter the category name: ";
         std::cin >> category_input;
-        categories[count++] = category_input;
+        categories[category_input];  //Adding categories to the map
+        std::cout << std::endl;
     }
 
-    std::cout << "Category(s) added successfully" << std::endl;
+    std::cout << "Category(s) added successfully\n" << std::endl;
 }
