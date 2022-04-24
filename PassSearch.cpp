76e210c95e9937d8148e086407c7720dd82efcd9
ace-krypton <ramizna@code.edu.az>
2022-04-24 18:17:03 +0200

    Added `flow control`

---
+++ PassSearch.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "header.hpp"
+
+
+auto pass_search() -> void {
+    std::cout << "----Password search will return passwords  matching the specified criteria----" << std::endl;
+    std::cout << "Enter the password you wanna search: ";
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <string>
 #include "header.hpp"
 
 auto main() -> int {
+    std::string user_input_str;  //User input (string)
+    int user_input_int;  //User input (integer)
+    bool main_loop = true;  //Boolean for main loop
     main_menu();  //Calling main_menu function for printing the menu
-    
+    std::cout << "> ";
+    std::cin >> user_input_int;  //Input from the user
+
+    while (main_loop) {  //Main loop
+        switch (user_input_int) {  //Main switch
+            case 0:  //Exit
+                std::cout << "Exiting..." << std::endl;
+                main_loop = false;
+                break;
+
+            case 1:  //Search for passwords
+                break;
+
+            case 2:  //Sort passwords
+                break;
+
+            case 3:  //Add a password
+                break;
+
+            case 4:  //Edit a password
+                break;
+
+            case 5:  //Remove a password
+                break;
+
+            case 6:  //Add a category
+                break;
+
+            case 7:  //Remove a category
+                break;
+
+            default:
+                std::cout << "Invalid command, Please try again";
+        }
+    }
+
     return 0;
 }
 
@@ -19,4 +58,5 @@ auto main_menu() -> void {
     std::cout << "[5] Remove a password" << std::endl;
     std::cout << "[6] Add a category" << std::endl;
     std::cout << "[7] Remove a category" << std::endl;
+    std::cout << "[0] Exit" << std::endl;
 }
