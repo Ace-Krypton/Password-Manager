942cc5d18f8c27209034e03b54fcba20d457b270
ace-krypton <ramizna@code.edu.az>
2022-04-24 17:27:28 +0200

    Testing on `algorithms` for encryption

--- main.cpp
+++ main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <fstream>
 
-auto main() -> int {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+int main ()
+{
+    int ans, key;
+    string in;
+    cout<<"Press 1 to Encrypt file.\nPress 2 to Decrypt file."<<endl;
+    cin>>ans;
+    cout<<"Enter File Name -"<<endl;
+    cin>>in;
+    cout<<"Enter Key -"<<endl;
+    cin>>key;
+    if (ans == 1)
+    {
+        char c;
+        ifstream fin;
+        ofstream fout;
+        fin.open(in.c_str(), ios::binary);
+        in = "output";
+        fout.open(in.c_str(), ios::binary);
+        while (!fin.eof())
+        {
+            fin>>noskipws>>c;
+            int temp = (c + key);
+            fout<<(char)temp;
+        }
+        fin.close();
+        fout.close();
+    }
+    else if (ans == 2)
+    {
+        char c;
+        ifstream fin;
+        ofstream fout;
+        fin.open(in.c_str(), ios::binary);
+        in = "qutput";
+        fout.open(in.c_str(), ios::binary);
+        while (!fin.eof())
+        {
+            fin>>noskipws>>c;
+            int temp = (c - key);
+            fout<<(char)temp;
+        }
+        fin.close();
+        fout.close();
+    }
 }
