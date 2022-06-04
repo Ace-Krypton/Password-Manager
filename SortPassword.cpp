23cfc882f0e929941a8a694d1d83ffd1aafe330e
ace-krypton <ramizna@code.edu.az>
2022-06-04 18:09:07 +0200

    Added `password sorting`

--- SortPassword.cpp
+++ SortPassword.cpp
@@ -19,6 +19,7 @@
 auto SortPassword::sort_password() -> void {
     std::multimap<int, std::string> sorted_password_list;
     std::vector<std::pair<int, std::map<std::string, std::string>>> elems(AddCategory::passwords_with_url.begin(), AddCategory::passwords_with_url.end());
+    std::vector<std::pair<std::string, std::vector<std::string>>> elems2(AddCategory::categories.begin(), AddCategory::categories.end());
 
     for (auto &it : AddCategory::passwords) {
         sorted_password_list.insert({ it.first, it.second });
@@ -27,12 +28,22 @@ auto SortPassword::sort_password() -> void {
     for (auto const &it : sorted_password_list) {
         std::cout << it.first << ": " << it.second << std::endl;
     }
+
     std::sort(elems.begin(), elems.end());
 
     for (auto const &it : elems) {
         std::cout << "[*] " << it.first << ": ";
         for (auto const &it2 : it.second) {
-            std::cout << it2.first << it2.second << std::endl;
+            std::cout << it2.first << ": " << it2.second << std::endl;
+        }
+    }
+
+    std::sort(elems2.begin(), elems2.end());
+
+    for (auto const &it : elems2) {
+        std::cout << "[*] " << it.first << ": ";
+        for (auto const &it2 : it.second) {
+            std::cout << it2 << std::endl;
         }
     }
 }
